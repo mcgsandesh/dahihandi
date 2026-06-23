4ba8e564ab371df333f536fe6289cae37cdd19f4
mcgsandesh <maharashtrachagovinda@gmail.com>
2026-06-23 20:20:25 +0530

    Vite PWA plugin config added successfully

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'DahiHandi Management System',
+        short_name: 'DahiHandi APP',
+        description: 'Manage teams, t-shirt reports, and insurance details easily.',
+        theme_color: '#ff6600', // आपला ऑरेंज ब्रँड कलर
+        background_color: '#f8fafc',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
